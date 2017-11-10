5a33ceeff1d49735aebf9e93077526982202ae89
Marcos Felipe <p.marcosfelipe@gmail.com>
2017-11-10 15:19:57 -0200

    Signed-off-by: Marcos Felipe <p.marcosfelipe@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,46 @@
 import '../css/main.css'
 import Vue from 'vue'
 import App from './App.vue'
+import GameBoard from './GameBoard.vue'
+
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const NotFound = { template: '<p>Página não encontrada!</p>' }
 
 window._ = require('lodash');
+
+const routes = {
+	'/': App,
+	'/game-board': GameBoard
+};
+
+var router = new VueRouter();
+
+router.map({
+    '': {
+        component: App
+    }
+});
 
 
 new Vue({
 	el: "#app",
-	render: h => h(App)
+	// render: h => h(App)
+
+	data: {
+		currentRoute: window.location.pathname
+	},
+
+	computed: {
+		ViewComponent () {
+			return routes[this.currentRoute] || NotFound
+		}
+	},
+
+	render (h) { return h(this.ViewComponent) }
 });
